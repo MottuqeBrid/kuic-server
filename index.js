4804a74daabdd0d2ab3ecab333990c35b0e4a7d6
Mottuqe Brid <mottuqebrid@gmail.com>
2025-09-21 22:39:18 +0600

    feat: add cookie-parser middleware and update dependencies

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// middleware
+
+app.use(cookieParser());
+
+app.set("trust proxy", 1); // <--- important for proxies
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://kuic.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://kuic-server.vercel.app",
+      "https://kuic.vercel.app",
+    ],
     credentials: true,
   })
 );
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGO_URL);
+// database connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
+// routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
